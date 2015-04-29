3f66709de51086eda46dcbabd6e71219821cf28d
koneriranjith <ranjithk.cn@gmail.com>
2015-04-29 16:56:46 +0530

    Some commit message

---
+++ client/app/admin/admin.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('projectTwoApp')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/admin', {
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
